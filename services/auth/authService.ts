bfe8e744ed27f98c7dd625c7bdac4fc55f64b65c
Max Stryhun <horollgame@gmail.com>
2021-01-25 23:10:14 +0200

    TypeScript and beggin implametation class

---
+++ services/auth/authService.ts
@@ -0,0 +1,13 @@
+export class AuthService {
+    private readonly response;
+    private readonly request;
+
+    constructor(res: any, req: any) {
+        this.response = res;
+        this.request = res;
+    }
+
+    checkLogin() {
+
+    }
+}
